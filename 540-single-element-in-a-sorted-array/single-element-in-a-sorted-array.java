291f85cd445503f8b07017a3b9361a68000180bd
Malavika Manoj Pillai <151729679+malavikapillai@users.noreply.github.com>
2025-08-12 12:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 50.7 MB (21.28%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+              // Check boundaries before accessing mid-1 or mid+1
+            if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
+                (mid == nums.length - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+            // even half
+            if(mid%2==0)
+            {
+                if(nums[mid]==nums[mid-1])
+                {
+                    end=mid-1;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+
+            }
+            else
+            //odd half
+            {
+                if(nums[mid]==nums[mid-1])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+
+            }  
+
+        }
+        return -1;
+
+        
+    }
+}
