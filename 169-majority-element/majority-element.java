e9edb16241e75d12d49fa2ba4b64ffde8bfdd942
Malavika Manoj Pillai <151729679+malavikapillai@users.noreply.github.com>
2026-02-07 19:16:47 +0530

    Time: 2 ms (71.40%) | Memory: 55.8 MB (44.75%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,26 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    // BOYER MOORE VOTING ALGO
-    int majo=nums[0];
-    int count=0;
-
-
-    for(int i=0;i<nums.length;i++)
-    {
-        if(count==0)
+        
+        int vote=0;
+        int majo=nums[0];
+        for(int i=0;i<nums.length;i++)
         {
-            majo=nums[i];
+            if(vote==0)
+            {
+                majo=nums[i];  
+            }
+            if(nums[i]==majo)
+            {
+                vote++;
+            }
+            else
+            {
+                vote--;
+            }
         }
-        if(nums[i]==majo)
-        {
-            count++;
-        }
-        else
-        {
-            count--;
-        } 
-    }
-    return majo;
-
+        return majo;
     }
 }
