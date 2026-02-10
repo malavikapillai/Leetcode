ce7a7ded0b0782eebb5eac0c434c04ead486481b
Malavika Manoj Pillai <151729679+malavikapillai@users.noreply.github.com>
2026-02-10 10:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (95.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,7 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
+
         int start=0;
         int end=nums.length-1;
+        int n=nums.length;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[end])
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid;
+            }
+        }
+
+        int pivot=end;
+        int left=binary(nums,0,pivot-1,target);
+        if(left!=-1) return left;
+        int right=binary(nums,pivot,n-1,target);
+        return right;
+
+    }
+    public int binary(int nums[],int start,int end,int target)
+    {
         while(start<=end)
         {
             int mid=start+(end-start)/2;
@@ -9,36 +33,16 @@ class Solution {
             {
                 return mid;
             }
-
-            // check which half is sorted
-            if(nums[start]<=nums[mid])
+            else if(nums[mid]>target)
             {
-                if(target>=nums[start] && target<nums[mid])
-                {
-                    end=mid-1;
-
-                }
-                else
-                {
-                    start=mid+1;
-                }
-            
+                end=mid-1;
             }
             else
-            // right half is sorted
             {
-                if(target>nums[mid]&& target<=nums[end])
-                {
-                    start=mid+1;
-                }
-                else
-                {
-                    end=mid-1;
-                }
-
+                start=mid+1;
             }
+
         }
         return -1;
-      
     }
 }
